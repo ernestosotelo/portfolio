bcd59ba2d69eda47b488ca1f14558058202e8597
Ernesto <ernesto.sotelo@hotmail.com>
2021-04-03 10:25:15 -0400

    new

--- pages/projects/tynker.js
+++ pages/projects/tynker.js
@@ -1,51 +1,51 @@
-import React from 'react'
-import Head from 'next/head'
-import Navigation from '../../components/Navigation'
-import styles from '../../styles/Project.module.scss'
+// import React from 'react'
+// import Head from 'next/head'
+// import Navigation from '../../components/Navigation'
+// import styles from '../../styles/Project.module.scss'
 
-export default function Tynker() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Projects - Tynker | Ernesto Sotelo</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// export default function Tynker() {
+//   return (
+//     <div className="container">
+//       <Head>
+//         <title>Projects - Tynker | Ernesto Sotelo</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <Navigation />
+//       <Navigation />
 
-      <div className="main-content">
-        <h1 className="title">Project Details</h1>
-        <img
-          className={styles.projectImg}
-          src="/project-tynker.svg"
-          alt="Graphic for the Tynker project."
-        />
+//       <div className="main-content">
+//         <h1 className="title">Project Details</h1>
+//         <img
+//           className={styles.projectImg}
+//           src="/project-tynker.svg"
+//           alt="Graphic for the Tynker project."
+//         />
 
-        <h2 className={styles.annotationHeader}>Context</h2>
-        <p className={styles.annotationBody}>
-          This project was worked on as part of a class project.
-        </p>
-        <h2 className={styles.annotationHeader}>Conditions</h2>
-        <p className={styles.annotationBody}>
-          Project was completed in about 2 weeks. Graphics used are original
-          with the exception of logo or branding that was added in reference of
-          Tynker. The intent of this project was to develop an instructional
-          video demonstrating how a specific educational technology is used. I
-          selected Tynker, and then constructed the instructional video.{' '}
-        </p>
-        <h2 className={styles.annotationHeader}>Scope</h2>
-        <p className={styles.annotationBody}>
-          This project was completed, and could be revisited for further editing
-          and improved video enhancements (such as more graphics), and improved
-          content structuring / voiceover.
-        </p>
-        <h2 className={styles.annotationHeader}>Role</h2>
-        <p className={styles.annotationBody}>
-          Overall, I recorded the video using screencasting software, created
-          the graphics using Adobe software, and lastly performed video editing
-          using Adobe Premiere.
-        </p>
-      </div>
-    </div>
-  )
-}
+//         <h2 className={styles.annotationHeader}>Context</h2>
+//         <p className={styles.annotationBody}>
+//           This project was worked on as part of a class project.
+//         </p>
+//         <h2 className={styles.annotationHeader}>Conditions</h2>
+//         <p className={styles.annotationBody}>
+//           Project was completed in about 2 weeks. Graphics used are original
+//           with the exception of logo or branding that was added in reference of
+//           Tynker. The intent of this project was to develop an instructional
+//           video demonstrating how a specific educational technology is used. I
+//           selected Tynker, and then constructed the instructional video.{' '}
+//         </p>
+//         <h2 className={styles.annotationHeader}>Scope</h2>
+//         <p className={styles.annotationBody}>
+//           This project was completed, and could be revisited for further editing
+//           and improved video enhancements (such as more graphics), and improved
+//           content structuring / voiceover.
+//         </p>
+//         <h2 className={styles.annotationHeader}>Role</h2>
+//         <p className={styles.annotationBody}>
+//           Overall, I recorded the video using screencasting software, created
+//           the graphics using Adobe software, and lastly performed video editing
+//           using Adobe Premiere.
+//         </p>
+//       </div>
+//     </div>
+//   )
+// }
